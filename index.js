390294c4d4fb027dd8fdcdc9d9a765cf3a4c4dbe
palakons <palakons@gmail.com>
2017-04-07 18:31:04 +0700

    update input

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var https = require('https');
 var url = require('url');
 var extend = require('extend');
 var URL = require('url').URL;
+var ddbTable = process.env.TRAINING_IMAGE_TABLE || 'chronos-palakons';
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
@@ -39,7 +40,7 @@ if (cluster.isMaster) {
 
 
 
-    var port = process.env.PORT ||3000;
+    var port = process.env.PORT || 3000;
 
     var server = app.listen(port, function () {
         console.log('Server running at http://127.0.0.1:' + port + '/');
@@ -47,19 +48,38 @@ if (cluster.isMaster) {
 
     app.get('/', function (req, res) {
         //console.log('Called form User-Agent: ' + req.headers['user-agent']);
-        console.log('Called / form User-Agent: ' + JSON.stringify(req.headers,null,2));
+        console.log('Called / form User-Agent: ' + JSON.stringify(req.headers, null, 2));
         res.render('index', {
             static_path: 'static',
             theme: process.env.THEME || 'flatly',
             flask_debug: process.env.FLASK_DEBUG || 'false'
         });
     });
-    app.get('/healthcheck',function (req, res) {
-        console.log('Called /healthcheck form User-Agent: ' + JSON.stringify(req.headers,null,2));
-        res.status(200).json(extend({ 'status': 'successful' },req.body));
+    app.get('/healthcheck', function (req, res) {
+        console.log('Called /healthcheck form User-Agent: ' + JSON.stringify(req.headers, null, 2));
+        res.status(200).json(extend({ 'status': 'successful' }, req.body));
     });
     app.post('/update', function (req, res) {
-        console.log('Called /update form User-Agent: ' + JSON.stringify(req.headers,null,2));
-        res.status(200).json(extend({ 'status': 'successful' },req.body));
+        console.log('Called /update form User-Agent: ' + JSON.stringify(req.headers, null, 2));
+        let data = { time_ms: { N: Date.now(), cpu_id: { S: req.body.id }, status: { S: req.body.status } } };
+        res.status(200).json(extend(putinDB(ddb, ddbTable, data), req.body));
     });
+    function putinDB(dDB, ddbTableName, data) {
+
+        var params = {
+            'TableName': ddbTableName,
+            'Item': data
+        };
+        console.log('Data to in put: ',JSON.stringify(params, null, 2));
+        dDB.putItem(
+            params
+            , function (err, data) {
+                if (err) {
+                    return extend({ status: error }, err);
+                    console.log('DDB Error: ' + err);
+                } else {
+                    return { status: success };
+                }
+            });
+    }
 }
